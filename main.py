f4ab8a3910db4e26f06a920d5f3606dc34be6b50
olive <oliver@dittert.org>
2025-05-10 16:13:32 +0200

    C: Practice commit

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #aus constants
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
     while True:
@@ -12,11 +13,12 @@ def main():
             if event.type == pygame.QUIT:
                 return    
         
-        screen.fill((0, 0, 0))
-        pygame.display.flip()
+        screen.fill((0, 0, 0))              # Black Screen
+        player.draw(screen)                 # re-render the player on the screen each frame
+
+        pygame.display.flip()               
         
-        # limit the framerate to 60 FPS
-        dt = clock.tick(60) / 1000 
+        dt = clock.tick(60) / 1000          # limit the framerate to 60 FPS
 
 if __name__ == "__main__":
     main()
---
+++ player.py
@@ -0,0 +1,16 @@
+class Player(CircleShape):                  
+    def __init__(x, y):                     
+        super().__init__(x, y, radius)      # ???
+        self.radius = player_radius         # ???
+        rotation = 0
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    def draw(self, screen):
+        pygame.draw.polygon(screen, white, self.triangle(), width=2)
