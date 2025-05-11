25c3b4bd72ad64cb5233b711909b0cf60c8aebbc
olive <oliver@dittert.org>
2025-05-11 11:37:31 +0200

    E: Final

---
+++ asteroid.py
@@ -0,0 +1,38 @@
+import pygame
+import random
+from constants import *
+from circleshape import CircleShape
+
+class Asteroid(CircleShape):
+    def _init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+
+# Override the update() method so that it moves in a straight line at constant speed. 
+# On each frame, it should add (self.velocity * dt) to its position 
+# (get self.velocity from its parent class, CircleShape).
+
+    def update(self, dt):
+        self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        random_angle = random.uniform(20, 50)
+        a = self.velocity.rotate(random_angle)          # Spawn Vector new asteroid
+        b = self.velocity.rotate(-random_angle)         # Spawn Vector new asteroid
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid.velocity = a * 1.2
+        asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid.velocity = b * 1.2
+        
+        
+            
--- main.py
+++ main.py
@@ -1,11 +1,28 @@
+import sys
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField 
+from shot import Shot
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #aus constants
     clock = pygame.time.Clock()
+    
+    updatable = pygame.sprite.Group()       # all the objects that can be updated
+    drawable = pygame.sprite.Group()        # all the objects that can be drawn
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()           # Initialization code that contains all of your shots.
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()        # Wo kommt das her?!
+    
+    Player.containers = (updatable, drawable)
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
@@ -14,14 +31,25 @@ def main():
             if event.type == pygame.QUIT:
                 return    
         
+        updatable.update(dt)                # Wo kommt das her?!
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+            for shot in shots:           
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split()
+
         screen.fill((0, 0, 0))              # Black Screen
-        player.draw(screen)                 # re-render the player on the screen each frame
+        
+        for obj in drawable:
+            obj.draw(screen)                 # re-render the player on the screen each frame
+                
         pygame.display.flip()               
         
         dt = clock.tick(60) / 1000          # limit the framerate to 60 FPS
 
 if __name__ == "__main__":
     main()
-
-print(f"Screen width: {SCREEN_WIDTH}")
-print(f"Screen height: {SCREEN_HEIGHT}")
